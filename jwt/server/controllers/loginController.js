5ed442722d9e0a87947b1b15e4130ec12a7b0471
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-09-04 23:19:49 +0200

    docs : comments added

--- jwt/server/controllers/loginController.js
+++ jwt/server/controllers/loginController.js
@@ -3,11 +3,14 @@ const bcrypt = require('bcrypt')
 
 
 async function loginController(req, res) {
+    // Getting the req body
     const { name, email, password } = req.body
 
-    const user = await UserModel.findOne({ email })
+    // Checking if the user's email and name are valid
+    const user = await UserModel.findOne({ email, name })
     if (!user) res.status(400).send('Email is not found')
 
+    // Checking if the password hash is the same as the one entered by the user when login
     const validPassword = await bcrypt.compare(password, user.password)
     if (!validPassword) return res.status(400).send('Invalid password')
 
