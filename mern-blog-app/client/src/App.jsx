795d8f1c99f6ce4069e1e712053c5db4c45496cf
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-09-20 11:42:37 +0200

    feat : adding layouts and started working on styling

--- mern-blog-app/client/src/App.jsx
+++ mern-blog-app/client/src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './pages/login.jsx'
+import Register from './pages/register'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-blue-500'>Hello world</h1>
-    </div>
+      <div className='font-inter'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
   )
 }
 
---
+++ mern-blog-app/client/src/pages/register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='flex w-full'>
+      <div className='bg-topography'></div>
+      <div 
+      className='text-7xl font-extrabold bg-waves h-screen w-1/2'
+      >register</div>
+    </div>
+  )
+}
+
+export default Register
