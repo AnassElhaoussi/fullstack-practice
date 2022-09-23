9802d667a874485520520290920f6115ba6d0d87
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-09-23 17:21:33 +0200

    feat : completing register component

--- mern-blog-app/client/src/pages/register.jsx
+++ mern-blog-app/client/src/pages/register.jsx
@@ -1,20 +1,70 @@
-import React from 'react'
-import {Heading, Stack} from '@chakra-ui/react'
+import React, {useState} from 'react'
+import {Heading, Button, Divider, Stack, Input, InputGroup, InputLeftElement, InputRightElement} from '@chakra-ui/react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUserPlus, faKeyboard, faEnvelope, faLock, faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
 
 const Register = () => {
+  const [isPasswordShown, setIsPasswordShown] = useState(false)
   return (
     <div className='flex w-full'>
       <div className='bg-topography'></div>
       <Stack 
-      height='100vh' 
+      height='100vh'
       width='50%' 
-      alignItems='center' 
+      alignItems='start'
+      gap='2rem'
+      px={20} 
       justifyContent='center' 
       textAlign='center'>
-        <Heading
-        as='h1' size='4xl' fontWeight={800} color='blue.500'
-        >feekrah</Heading>
-        <Heading>Register to feekrah</Heading>
+        <Stack gap='0.5rem' alignItems='start'>
+          <FontAwesomeIcon 
+          icon={faUserPlus} 
+          className='text-violet-700 text-2xl' />
+          <Heading
+          as='h3' size='lg' fontWeight={800}
+          >Register an account</Heading>
+          <Divider border='8px' borderRadius='1rem' width='10%' borderColor='blue' />
+        </Stack>
+        <Stack gap='0.5rem'>
+          <InputGroup>
+            <InputLeftElement 
+            pointerEvents='none' 
+            children={<FontAwesomeIcon icon={faKeyboard} />} />
+            <Input 
+            type='text' 
+            placeholder='Enter a username' 
+            focusBorderColor='blueviolet' />
+          </InputGroup>
+          <InputGroup>
+            <InputLeftElement 
+            pointerEvents='none' 
+            children={<FontAwesomeIcon icon={faEnvelope} />} />
+            <Input 
+            type='email' 
+            placeholder='Enter an email'
+            focusBorderColor='blueviolet'
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputLeftElement 
+            pointerEvents='none' 
+            children={<FontAwesomeIcon icon={faLock} />} />
+            <Input 
+            type={isPasswordShown ? 'text' : 'password'}
+            placeholder='Create a password'
+            focusBorderColor='blueviolet'
+            />
+            <InputRightElement 
+            cursor='pointer' 
+            children={isPasswordShown 
+              ? <FontAwesomeIcon icon={faEyeSlash} /> 
+              : <FontAwesomeIcon icon={faEye} />}
+            onClick={() => setIsPasswordShown(!isPasswordShown)}
+            userSelect='none'
+            />
+          </InputGroup>
+          <Button colorScheme='purple' className='shadow-lg'>Submit</Button>
+        </Stack>
       </Stack>
     </div>
   )
